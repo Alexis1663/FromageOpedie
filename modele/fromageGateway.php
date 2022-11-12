184096d258e6ebdb3fd72e0b8ba5da752fa31d4c
LouBRODA <115875561+LouBRODA@users.noreply.github.com>
2022-11-12 06:44:48 +0100

    Add files via upload
    add connection.php & debut fromageGateway.php

---
+++ modele/fromageGateway.php
@@ -0,0 +1,68 @@
+<?php
+
+include 'fromage.php';
+include 'connection.php';
+
+class FromageGateway{
+
+	private $con;
+
+	public	function __construct(Connection	$con){	 
+		$this->con=$con;
+	}
+
+	public function findAll():array{
+		$queryfindall = "SELECT * FROM Fromage";
+		$this->con->executeQuery($queryfindall, array(':image'=>array($image,PDO:PARAM_STR), 'nom'=>array($nom,PDO:PARAM_STR), 'departementFabrication'=>array($departementFabrication,PDO:PARAM_STR) ));
+
+		$results=$this->con->getResults();
+		Foreach ($results as $row)
+		$Tab_Fromages_FindAll[]=new Article($row['image'],$row['nom'],$row['departementFabrication']);
+		Return $Tab_Fromages_FindAll;
+	}
+
+	public function find(string $nom){
+		$queryfind = "SELECT * FROM Article WHERE nom=:nom";
+		$this->con->executeQuery($queryfind,  array(':image'=>array($image,PDO:PARAM_STR), 'nom'=>array($nom,PDO:PARAM_STR), 'departementFabrication'=>array($departementFabrication,PDO:PARAM_STR) ));
+
+		$results=$this->con->getResults();
+		Foreach ($results as $row)
+		$Tab_Fromages_Find[]=new Article($row['image'],$row['nom'],$row['departementFabrication']);
+		Return $Tab_Fromages_Find;
+	}
+
+	public function sortZA():array{
+		$querysortza = "SELECT * FROM Fromage ORDER BY nom DESC";
+		$this->con->executeQuery($querysortza, array(':image'=>array($image,PDO:PARAM_STR), 'nom'=>array($nom,PDO:PARAM_STR), 'departementFabrication'=>array($departementFabrication,PDO:PARAM_STR) ));
+
+		$results=$this->con->getResults();
+		Foreach ($results as $row)
+		$Tab_Fromages_SortZA[]=new Article($row['image'],$row['nom'],$row['departementFabrication']);
+		Return $Tab_Fromages_SortZA;
+	}
+
+	public function sortDep():array{
+		$querysortdep = "SELECT * FROM Fromage ORDER BY departementFabrication ASC";
+		$this->con->executeQuery($querysortdep, array(':image'=>array($image,PDO:PARAM_STR), 'nom'=>array($nom,PDO:PARAM_STR), 'departementFabrication'=>array($departementFabrication,PDO:PARAM_STR) ));
+
+		$results=$this->con->getResults();
+		Foreach ($results as $row)
+		$Tab_Fromages_SortDep=new Article($row['image'],$row['nom'],$row['departementFabrication']);
+		Return $Tab_Fromages_SortDep;
+	}
+
+	public function sortLait():array{
+		$querysortlait = "SELECT * FROM Fromage ORDER BY lait ASC";
+		$this->con->executeQuery($querysortlait, array(':image'=>array($image,PDO:PARAM_STR), 'nom'=>array($nom,PDO:PARAM_STR), 'departementFabrication'=>array($departementFabrication,PDO:PARAM_STR) ));
+
+		$results=$this->con->getResults();
+		Foreach ($results as $row)
+		$Tab_Fromages_SortLait=new Article($row['image'],$row['nom'],$row['departementFabrication']);
+		Return $Tab_Fromages_SortLait;
+	}
+
+	//public function sortNote():array;
+
+}
+
+?>
