5f4795f81f89b887bd34442077a52da1e6ea47ac
Misticku <114564898+Misticku@users.noreply.github.com>
2022-11-14 17:32:09 +0100

    ajout du accueilGateway dans le controleur accueil

--- controleur/accueilControleur.php
+++ controleur/accueilControleur.php
@@ -6,14 +6,17 @@ require_once 'vue/accueil.php';
 class AccueilControleur {
 
   private $fromageG;
+  private $accueilG;
 
   public function __construct() {
     $this->fromageG = new FromageGateway();
+    $this->accueilG = new accueilGateway();
   }
 
   // Affiche la liste de tous les fromages de la bdd
   public function accueil() {
     $lesFromages = $this->fromageG->findAll();
+    
   }
 
 }
