e6c60a126d77c411ed0b1714c48268e533fc5e05
Jérémy BESSON <114564898+Misticku@users.noreply.github.com>
2022-11-18 11:04:18 +0100

    ajout de fonction sur le fromageControler

--- controleur/fromageControleur.php
+++ controleur/fromageControleur.php
@@ -1,42 +1,47 @@
 <?php
 
-require_once('modele/fromageGateway.php');
-require_once('modele/connection.php');
+require_once 'modele/fromageGateway.php';
+require_once 'vue/fromage.php';
 
-class FromageControleur
-{
+class FromageControleur {
 
-    private $fromageG;
+  private $fromageG;
 
-    private $conn;
+  public function __construct() {
+    $this->fromageG = new FromageGateway();
+  }
 
-    public function __construct($dns, $user, $password)
-    {
-        $this->conn = new Connection($dns, $user, $password);
-        $this->fromageG = new FromageGateway($this->conn);
-    }
+  // Affiche la liste de tous les fromages de la bdd
+  public function findAllFromages() {
+    $lesFromages = $this->fromageG->findAll();
+  }
 
-    // Affiche la liste de tous les fromages de la bdd
-    public function findAllFromages()
-    {
-        $lesFromages = $this->fromageG->findAll();
-    }
+  // Trie les fromages par ordre alphabétique inverse
+  public function sortZAFromages() {
+    $lesFromages = $this->fromageG->sortZA();
+  }
 
-    // Trie les fromages par ordre alphabétique inverse
-    public function sortZAFromages()
-    {
-        $lesFromages = $this->fromageG->sortZA();
-    }
+  // Trie les fromages par département dans l'ordre croissant
+  public function sortDepFromages() {
+    $lesFromages = $this->fromageG->sortDep();
+  }
 
-    // Trie les fromages par département dans l'ordre croissant
-    public function sortDepFromages()
-    {
-        $lesFromages = $this->fromageG->sortDep();
-    }
+  // Trie les fromages par type de lait dans l'ordre croissant
+  public function sortLaitFromages() {
+    $lesFromages = $this->fromageG->sortLait();
+  }
 
-    // Trie les fromages par type de lait dans l'ordre croissant
-    public function sortLaitFromages()
-    {
-        $lesFromages = $this->fromageG->sortLait();
-    }
+  // Trie les fromages par type de pâte dans l'ordre croissant
+  public function sortPateFromages() {
+    $lesFromages = $this->fromageG->sortPate();
+  }
+
+  // Affiche les détails d'un fromage
+  public function findFromage(string $nom) {
+    $leFromage = $this->fromageG->find($nom);
+  }
+
+
 }
+
+?>
